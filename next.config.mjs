7c0204c3876374545b68017f0ea6abf7cc2c75ed
aryanbelle <aryanbelle692@gmail.com>
2025-03-13 22:31:53 +0530

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['mongoose'],
-  },
-  output: 'standalone',
-  trailingSlash: false,
+  output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: false,
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
